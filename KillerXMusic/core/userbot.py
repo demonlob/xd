1745336022789eb9337a73308a13f7434f1e50b4
demonlob <142598456+demonlob@users.noreply.github.com>
2023-09-05 23:38:00 +0530

    Update userbot.py

--- KillerXMusic/core/userbot.py
+++ KillerXMusic/core/userbot.py
@@ -62,9 +62,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("KillerXSupport")
-                await self.one.join_chat("rendyprojects")
-                await self.one.join_chat("pantekyks")
+                await self.one.join_chat("Teamfoxbots")
+                await self.one.join_chat("Teamfoxbots")
+                await self.one.join_chat("Teamfoxbots")
             except:
                 pass
             assistants.append(1)
@@ -91,9 +91,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("KillerXSupport")
-                await self.two.join_chat("rendyprojects")
-                await self.two.join_chat("pantekyks")
+                await self.two.join_chat("Teamfoxbots")
+                await self.two.join_chat("Teamfoxbots")
+                await self.two.join_chat("Teamfoxbots")
             except:
                 pass
             assistants.append(2)
@@ -120,9 +120,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("KillerXSupport")
-                await self.three.join_chat("rendyprojects")
-                await self.three.join_chat("pantekyks")
+                await self.three.join_chat("Teamfoxbots")
+                await self.three.join_chat("Teamfoxbots")
+                await self.three.join_chat("Teamfoxbots")
             except:
                 pass
             assistants.append(3)
@@ -149,9 +149,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("KillerXSupport")
-                await self.four.join_chat("rendyprojects")
-                await self.four.join_chat("pantekyks")
+                await self.four.join_chat("Teamfoxbots")
+                await self.four.join_chat("Teamfoxbots")
+                await self.four.join_chat("Teamfoxbots")
             except:
                 pass
             assistants.append(4)
@@ -178,9 +178,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("KillerXSupport")
-                await self.five.join_chat("rendyprojects")
-                await self.five.join_chat("pantekyks")
+                await self.five.join_chat("Teamfoxbots")
+                await self.five.join_chat("Teamfoxbots")
+                await self.five.join_chat("Teamfoxbots")
             except:
                 pass
             assistants.append(5)
